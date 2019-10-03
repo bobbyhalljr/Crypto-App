f8dbf73606d60f8487b5b2796820cb13002703b5
Bobby Hall Jr <bobbyhalljr@Bobbys-MacBook.local>
2019-10-03 00:48:41 -0400

    added material ui, rendered coins

--- crypto-ex/src/App.js
+++ crypto-ex/src/App.js
@@ -1,11 +1,24 @@
 import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
+
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import Coins from './components/Coins';
+import Markets from './components/Markets';
 
 import './App.css';
 
 function App() {
+
   return (
-    <div className="App">
-      <h1>hello from app.js</h1>
+    <div>
+      <div className="App">
+        <Navbar />
+      </div>
+
+      <Route exact path='/' render={() => <Home />}/>
+      <Route path='/coins' render={() => <Coins />}/>
+      <Route path='/markets' render={() => <Markets />}/>
     </div>
   );
 }
---
+++ crypto-ex/src/components/Markets.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Markets = () => {
+    return (
+        <h1>markets.js</h1>
+    )
+}
+
+export default Markets;
